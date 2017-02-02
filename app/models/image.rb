7fddc4154ead68ebcbe2a4f4c4d6218149571bb4
joshkestenberg <joshkestenberg@gmail.com>
2017-02-02 16:57:19 -0500

    added validations and associated error messages

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,12 @@
 class Image < ApplicationRecord
-  
+  validates :artist, presence: true
+  validates :title, length: {minimum:3, maximum:20}
+  validates :url, length: {minimum:1}
+  validates :url, uniqueness: true
+  validates_format_of :url, :with => URI::regexp(%w(http https))
+
+  # private
+  # def url_starts_http
+  #   if Image.url
+  #   end
 end
